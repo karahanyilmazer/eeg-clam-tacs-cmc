720be148943bb171822fe7ec28835a53aac0493c
Karahan Yilmazer <karahan.yilmazer@tum.de>
2024-05-08 14:27:19 +0200

    style: Black formatting

--- process_calibration_no_stim.py
+++ process_calibration_no_stim.py
@@ -1,53 +1,80 @@
-import numpy as np
-import mne
-from scipy.io import savemat
-from mne.time_frequency import psd_array_welch
-import matplotlib.pyplot as plt
-from scipy.signal import filtfilt
-from scipy.stats import zscore
-from scipy.signal import hilbert
-from scipy import stats
 import tkinter as tk
 from tkinter import filedialog
-from scipy.signal import firwin
-from mne.filter import filter_data
-from mne.viz import plot_topomap
+
+import matplotlib.pyplot as plt
+import mne
+import numpy as np
 from mne.channels import make_standard_montage
-from scipy.signal import hilbert, detrend
-from mne.filter import next_fast_len
-from scipy.io import loadmat
-from mne.filter import create_filter
+from mne.time_frequency import psd_array_welch
+from mne.viz import plot_topomap
 from scipy import linalg
-from utils import plot_flip
+from scipy.io import savemat
+
+# from utils import plot_flip
+
 
 def wrap(phases):
     return (phases + np.pi) % (2 * np.pi) - np.pi
 
+
 def get_P_TARGET(raw):
-    raw.set_montage(make_standard_montage('easycap-M1'),match_case=False)
-    data_signal = raw.copy().filter(l_freq,h_freq, l_trans_bandwidth = 1, h_trans_bandwidth = 1)._data #alpha
-    data_noise = raw.copy().filter(8,16, l_trans_bandwidth = 1, h_trans_bandwidth = 1).filter(14,10, l_trans_bandwidth = 1, h_trans_bandwidth = 1)._data
-    data_broad = raw.copy().filter(1,30, l_trans_bandwidth = 1, h_trans_bandwidth = 1)._data
+    raw.set_montage(make_standard_montage('easycap-M1'), match_case=False)
+    data_signal = (
+        raw.copy()
+        .filter(l_freq, h_freq, l_trans_bandwidth=1, h_trans_bandwidth=1)
+        ._data
+    )  # alpha
+    data_noise = (
+        raw.copy()
+        .filter(8, 16, l_trans_bandwidth=1, h_trans_bandwidth=1)
+        .filter(14, 10, l_trans_bandwidth=1, h_trans_bandwidth=1)
+        ._data
+    )
+    data_broad = (
+        raw.copy().filter(1, 30, l_trans_bandwidth=1, h_trans_bandwidth=1)._data
+    )
     A = np.cov(data_signal)
     B = np.cov(data_noise)
-    evals, evecs = linalg.eig(A,B)
+    evals, evecs = linalg.eig(A, B)
     ix = np.argsort(evals)[::-1]
     D = evecs[:, ix].T
     M = linalg.pinv(D)
     for ix_comp in range(4):
-        psd,freqs = psd_array_welch(D[ix_comp]@data_broad,raw.info['sfreq'],fmin=1,fmax=30,n_fft=int(3*raw.info['sfreq']))
-        freq_mask = np.logical_and(freqs>l_freq,freqs<h_freq)
+        psd, freqs = psd_array_welch(
+            D[ix_comp] @ data_broad,
+            raw.info['sfreq'],
+            fmin=1,
+            fmax=30,
+            n_fft=int(3 * raw.info['sfreq']),
+        )
+        freq_mask = np.logical_and(freqs > l_freq, freqs < h_freq)
         peak_freq = freqs[freq_mask][np.argmax(psd[freq_mask])]
-        plt.semilogy(freqs,psd)
-        plt.title('{:d}, pf = {:.2f} Hz'.format(ix_comp,peak_freq))
-        plot_topomap(M[:,ix_comp],raw.info)
+        plt.semilogy(freqs, psd)
+        plt.title('{:d}, pf = {:.2f} Hz'.format(ix_comp, peak_freq))
+        plot_topomap(M[:, ix_comp], raw.info)
         plt.show()
-    return M[:,3],peak_freq
+    return M[:, 3], peak_freq
+
 
 montage = make_standard_montage('easycap-M1')
 
 # CHANGE HERE BAD ELECTRODES !!!!
-bads = ['P2','PO4', 'P4','AF4','F2','F4','O2','C5','O1','F10','TP9','F7','FT10','T8']
+bads = [
+    'P2',
+    'PO4',
+    'P4',
+    'AF4',
+    'F2',
+    'F4',
+    'O2',
+    'C5',
+    'O1',
+    'F10',
+    'TP9',
+    'F7',
+    'FT10',
+    'T8',
+]
 ### CHECK IF AFZ GOOD OR BAD!
 
 l_freq = 10
@@ -56,36 +83,38 @@ h_freq = 14
 # Load the data without stimulation
 root = tk.Tk()
 root.withdraw()
-folder_path = filedialog.askdirectory(initialdir = "C:\\Users\\David\\Documents\\Closed-Loop-amtACS-Validation\\Data\\12_05_22")
-raw = mne.io.read_raw_brainvision(folder_path+'\\Relax.vhdr',preload=True)
-raw.drop_channels(['envelope','envelope_am','force'])
+folder_path = filedialog.askdirectory(
+    initialdir="C:\\Files\\Coding\\Python\\Neuro\\eeg-clam-tacs-cmc\\data"
+)
+raw = mne.io.read_raw_brainvision(folder_path + '\\Relax.vhdr', preload=True)
+raw.drop_channels(['envelope', 'envelope_am', 'force'])
 
 # Save the indexes to exclude as well as the channels to use for the experiment
 exclude_idx = np.sort([raw.ch_names.index(ch) + 1 for ch in bads])
 data_dict = {"exclude_idx": exclude_idx}
 savemat(folder_path + "/exclude_idx.mat", data_dict)
 
-good_indices = np.where(~np.isin(raw.ch_names,bads))[0]
+good_indices = np.where(~np.isin(raw.ch_names, bads))[0]
 raw.drop_channels(bads)
-#Re-reference the data to parietal electrodes to attenuate occipital alpha components
-#raw_ref = raw.copy().set_eeg_reference(ref_channels=['Pz'])
+# Re-reference the data to parietal electrodes to attenuate occipital alpha components
+# raw_ref = raw.copy().set_eeg_reference(ref_channels=['Pz'])
 
-P_TARGET,peak_freq = get_P_TARGET(raw)
+P_TARGET, peak_freq = get_P_TARGET(raw)
 
-raw.filter(l_freq,h_freq)
-flip = plot_flip(raw,P_TARGET)
+raw.filter(l_freq, h_freq)
+# flip = plot_flip(raw,P_TARGET)
 plt.close()
 
 # Save P_TARGET_64 for matlab
-P_TARGET_64 = np.zeros((1,64))
-P_TARGET_64[0,good_indices] = P_TARGET
-data_dict = {'P_TARGET_64':P_TARGET_64}
-savemat(folder_path + "/P_TARGET_64.mat",data_dict)
+P_TARGET_64 = np.zeros((1, 64))
+P_TARGET_64[0, good_indices] = P_TARGET
+data_dict = {'P_TARGET_64': P_TARGET_64}
+savemat(folder_path + "/P_TARGET_64.mat", data_dict)
 
 # Save phase delay for matlab
-data_dict = {'phase_delay':0.0}
-savemat(folder_path + "/phase_delay.mat",data_dict)
+data_dict = {'phase_delay': 0.0}
+savemat(folder_path + "/phase_delay.mat", data_dict)
 
 # Save flip for matlab
-data_dict = {'flip':flip}
-savemat(folder_path + "/flip.mat",data_dict)
+# data_dict = {'flip': flip}
+# savemat(folder_path + "/flip.mat", data_dict)
--- test_ssd_beamformer.py
+++ test_ssd_beamformer.py
@@ -5,133 +5,186 @@ To compute SSD on xdf files - from the BeAM BCI
 
 """
 
-import glob
-import pyxdf
-import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
+import pyxdf
+
 matplotlib.use('Qt5Agg')
-from typing import List, Dict, Union
-from misc.LSLStreamInfoInterface import find_stream
-from misc.xdf2mne import stream2raw, marker_stream2events
-from scipy.io import loadmat
-from scipy import linalg
-from scipy.stats import sem
 import tkinter as tk
 from tkinter import filedialog
+
 import mne
 from mne.channels import make_standard_montage
 from mne.time_frequency import psd_array_welch
 from mne.viz import plot_topomap
+from scipy import linalg
+from scipy.io import loadmat
 from sklearn.cross_decomposition import CCA
-from scipy.stats import pearsonr
 
-def get_ssd_forward_model(raw): #SSD forward model - SNR
-    raw.set_montage(make_standard_montage('easycap-M1'),match_case=False)
-    data_alpha = raw.copy().filter(10,14,l_trans_bandwidth=1, h_trans_bandwidth=1)._data #
-    data_broad = raw.copy().filter(1,30,l_trans_bandwidth=1, h_trans_bandwidth=1)._data #,l_trans_bandwidth=1, h_trans_bandwidth=1
-    data_noise = raw.copy().filter(8,16,l_trans_bandwidth=1, h_trans_bandwidth=1).filter(14,10,l_trans_bandwidth=1, h_trans_bandwidth=1)._data
-    A = np.cov(data_alpha)#+np.cov(data_beta)
+
+def get_ssd_forward_model(raw):  # SSD forward model - SNR
+    raw.set_montage(make_standard_montage('easycap-M1'), match_case=False)
+    data_alpha = (
+        raw.copy().filter(10, 14, l_trans_bandwidth=1, h_trans_bandwidth=1)._data
+    )
+    data_broad = (
+        raw.copy().filter(1, 30, l_trans_bandwidth=1, h_trans_bandwidth=1)._data
+    )
+    data_noise = (
+        raw.copy()
+        .filter(8, 16, l_trans_bandwidth=1, h_trans_bandwidth=1)
+        .filter(14, 10, l_trans_bandwidth=1, h_trans_bandwidth=1)
+        ._data
+    )
+    A = np.cov(data_alpha)  # +np.cov(data_beta)
     B = np.cov(data_noise)
-    evals, evecs = linalg.eig(A,B)
+    evals, evecs = linalg.eig(A, B)
     ix = np.argsort(evals)[::-1]
     D = evecs[:, ix].T
     M = linalg.pinv(D)
 
     for ix_comp in range(5):
-        psd,freqs = psd_array_welch(D[ix_comp]@data_broad,raw.info['sfreq'],fmin=1,fmax=30,n_fft=int(3*raw.info['sfreq']))
-        freq_mask = np.logical_and(freqs>8,freqs<14)
+        psd, freqs = psd_array_welch(
+            D[ix_comp] @ data_broad,
+            raw.info['sfreq'],
+            fmin=1,
+            fmax=30,
+            n_fft=int(3 * raw.info['sfreq']),
+        )
+        freq_mask = np.logical_and(freqs > 8, freqs < 14)
         peak_freq = freqs[freq_mask][np.argmax(psd[freq_mask])]
-        #plt.figure()
-        plt.semilogy(freqs,psd)
-        plt.title('{:d}, pf = {:.2f} Hz'.format(ix_comp,peak_freq))
-        #plt.figure()
-        plt.title('{:d}, pf = {:.2f} Hz'.format(ix_comp,peak_freq))
-        plot_topomap(M[:,ix_comp],raw.info)
+        # plt.figure()
+        plt.semilogy(freqs, psd)
+        plt.title('{:d}, pf = {:.2f} Hz'.format(ix_comp, peak_freq))
+        # plt.figure()
+        plt.title('{:d}, pf = {:.2f} Hz'.format(ix_comp, peak_freq))
+        plot_topomap(M[:, ix_comp], raw.info)
         plt.show()
-    return M[:,0]
+    return M[:, 0]
+
 
 def plot_forward_model(raw, forward_model):
-    raw.set_montage(make_standard_montage('easycap-M1'),match_case=False)
-    data_broad = raw.copy().filter(1,30,l_trans_bandwidth=1, h_trans_bandwidth=1)._data #,l_trans_bandwidth=1, h_trans_bandwidth=1
-    psd, freqs = psd_array_welch(forward_model @ data_broad, raw.info['sfreq'], fmin=1, fmax=30,
-                                 n_fft=int(3 * raw.info['sfreq']))
+    raw.set_montage(make_standard_montage('easycap-M1'), match_case=False)
+    data_broad = (
+        raw.copy().filter(1, 30, l_trans_bandwidth=1, h_trans_bandwidth=1)._data
+    )  # ,l_trans_bandwidth=1, h_trans_bandwidth=1
+    psd, freqs = psd_array_welch(
+        forward_model @ data_broad,
+        raw.info['sfreq'],
+        fmin=1,
+        fmax=30,
+        n_fft=int(3 * raw.info['sfreq']),
+    )
     freq_mask = np.logical_and(freqs > 8, freqs < 14)
     peak_freq = freqs[freq_mask][np.argmax(psd[freq_mask])]
     plt.semilogy(freqs, psd)
     plt.title(peak_freq)
-    #plt.title(peak_freq)
+    # plt.title(peak_freq)
     plot_topomap(forward_model, raw.info)
     plt.show()
 
-def get_ssd_forward_model_old(raw): #SSD forward model - SNR
-    raw.set_montage(make_standard_montage('easycap-M1'),match_case=False)
-    data_alpha = raw.copy().filter(10,14)._data #
-    #data_beta
-    data_broad = raw.copy().filter(1,30)._data #,l_trans_bandwidth=1, h_trans_bandwidth=1
-    data_noise = raw.copy().filter(1,30).filter(14,10,l_trans_bandwidth=1, h_trans_bandwidth=1)._data
-    A = np.cov(data_alpha)#+np.cov(data_beta)
+
+def get_ssd_forward_model_old(raw):  # SSD forward model - SNR
+    raw.set_montage(make_standard_montage('easycap-M1'), match_case=False)
+    data_alpha = raw.copy().filter(10, 14)._data  #
+    # data_beta
+    data_broad = (
+        raw.copy().filter(1, 30)._data
+    )  # ,l_trans_bandwidth=1, h_trans_bandwidth=1
+    data_noise = (
+        raw.copy()
+        .filter(1, 30)
+        .filter(14, 10, l_trans_bandwidth=1, h_trans_bandwidth=1)
+        ._data
+    )
+    A = np.cov(data_alpha)  # +np.cov(data_beta)
     B = np.cov(data_noise)
-    evals, evecs = linalg.eig(A,B)
+    evals, evecs = linalg.eig(A, B)
     ix = np.argsort(evals)[::-1]
     D = evecs[:, ix].T
     M = linalg.pinv(D)
 
     for ix_comp in range(5):
-        psd,freqs = psd_array_welch(D[ix_comp]@data_broad,raw.info['sfreq'],fmin=1,fmax=30,n_fft=int(3*raw.info['sfreq']))
-        freq_mask = np.logical_and(freqs>8,freqs<14)
+        psd, freqs = psd_array_welch(
+            D[ix_comp] @ data_broad,
+            raw.info['sfreq'],
+            fmin=1,
+            fmax=30,
+            n_fft=int(3 * raw.info['sfreq']),
+        )
+        freq_mask = np.logical_and(freqs > 8, freqs < 14)
         peak_freq = freqs[freq_mask][np.argmax(psd[freq_mask])]
-        #plt.figure()
-        plt.semilogy(freqs,psd)
-        plt.title('{:d}, pf = {:.2f} Hz'.format(ix_comp,peak_freq))
-        #plt.figure()
-        plt.title('{:d}, pf = {:.2f} Hz'.format(ix_comp,peak_freq))
-        plot_topomap(M[:,ix_comp],raw.info)
+        # plt.figure()
+        plt.semilogy(freqs, psd)
+        plt.title('{:d}, pf = {:.2f} Hz'.format(ix_comp, peak_freq))
+        # plt.figure()
+        plt.title('{:d}, pf = {:.2f} Hz'.format(ix_comp, peak_freq))
+        plot_topomap(M[:, ix_comp], raw.info)
         plt.show()
-    return M[:,0]
-
+    return M[:, 0]
 
 
 def get_cca_forward_model(raw):
-    raw.set_montage('easycap-M1',match_case=False)
-    data_alpha = raw.copy().filter(10,14,l_trans_bandwidth=1,h_trans_bandwidth=1)._data
-    data_beta = raw.copy().filter(20,28,l_trans_bandwidth=1,h_trans_bandwidth=1)._data#.apply_hilbert(envelope=True).filter(10,14,l_trans_bandwidth=1,h_trans_bandwidth=1)._data
-    data_noise = raw.copy().filter(8,16,l_trans_bandwidth=1, h_trans_bandwidth=1).filter(14,10,l_trans_bandwidth=1, h_trans_bandwidth=1)._data
-    data_broad = raw.copy().filter(1,30)._data
+    raw.set_montage('easycap-M1', match_case=False)
+    data_alpha = (
+        raw.copy().filter(10, 14, l_trans_bandwidth=1, h_trans_bandwidth=1)._data
+    )
+    data_beta = (
+        raw.copy().filter(20, 28, l_trans_bandwidth=1, h_trans_bandwidth=1)._data
+    )  # .apply_hilbert(envelope=True).filter(10,14,l_trans_bandwidth=1,h_trans_bandwidth=1)._data
+    data_noise = (
+        raw.copy()
+        .filter(8, 16, l_trans_bandwidth=1, h_trans_bandwidth=1)
+        .filter(14, 10, l_trans_bandwidth=1, h_trans_bandwidth=1)
+        ._data
+    )
+    data_broad = raw.copy().filter(1, 30)._data
     n_chs = len(raw.ch_names)
     cca = CCA(n_components=n_chs)
-    cca.fit(data_alpha.T,data_beta.T)
+    cca.fit(data_alpha.T, data_beta.T)
     L = cca.x_loadings_
-    M = np.diag(np.std(data_alpha,axis=-1))@L
+    M = np.diag(np.std(data_alpha, axis=-1)) @ L
     fig, axs = plt.subplots(2, 5, figsize=(12, 4), layout='constrained')
     for ix_comp in range(5):
-        #psd,freqs = psd_array_welch(make_target(data_broad,M[:,ix_comp]),raw.info['sfreq'],fmin=1,fmax=30,n_fft=int(3*raw.info['sfreq']))
-        psd, freqs = psd_array_welch(data_broad@M[:, ix_comp], raw.info['sfreq'], fmin=1, fmax=30, n_fft=int(3 * raw.info['sfreq']))
-        freq_mask = np.logical_and(freqs>1,freqs<30)
+        # psd,freqs = psd_array_welch(make_target(data_broad,M[:,ix_comp]),raw.info['sfreq'],fmin=1,fmax=30,n_fft=int(3*raw.info['sfreq']))
+        psd, freqs = psd_array_welch(
+            data_broad @ M[:, ix_comp],
+            raw.info['sfreq'],
+            fmin=1,
+            fmax=30,
+            n_fft=int(3 * raw.info['sfreq']),
+        )
+        freq_mask = np.logical_and(freqs > 1, freqs < 30)
         peak_freq = freqs[freq_mask][np.argmax(psd[freq_mask])]
-        axs[1,ix_comp].semilogy(freqs,psd)
-        plot_topomap(M[:,ix_comp],raw.info,axes=axs[0,ix_comp],show=False)
-        axs[0,ix_comp].set_title('{:d}'.format(ix_comp))
+        axs[1, ix_comp].semilogy(freqs, psd)
+        plot_topomap(M[:, ix_comp], raw.info, axes=axs[0, ix_comp], show=False)
+        axs[0, ix_comp].set_title('{:d}'.format(ix_comp))
         plt.show()
-    return M[:,0]
+    return M[:, 0]
 
 
-def make_target(data,forward_model,COV=None):
+def make_target(data, forward_model, COV=None):
     n_chs = len(forward_model)
     if data.ndim == 3:
-        data = data[:,:n_chs]
+        data = data[:, :n_chs]
         if COV is None:
-            COV = np.cov(np.real(np.concatenate(data,axis=-1)))
+            COV = np.cov(np.real(np.concatenate(data, axis=-1)))
         COVinv = linalg.pinv(COV)
-        w = ((COVinv@forward_model[:,None])).squeeze()/(forward_model[None,:]@COVinv@forward_model[:,None])
-        return np.array([w@ep for ep in data]).squeeze()
+        w = ((COVinv @ forward_model[:, None])).squeeze() / (
+            forward_model[None, :] @ COVinv @ forward_model[:, None]
+        )
+        return np.array([w @ ep for ep in data]).squeeze()
     else:
         data = data[:n_chs]
         if COV is None:
             COV = np.cov(np.real(data))
         COVinv = linalg.pinv(COV)
-        w = ((COVinv@forward_model[:,None])).squeeze()/(forward_model[None,:]@COVinv@forward_model[:,None])
-        return (w@data).squeeze()
+        w = ((COVinv @ forward_model[:, None])).squeeze() / (
+            forward_model[None, :] @ COVinv @ forward_model[:, None]
+        )
+        return (w @ data).squeeze()
+
 
 def create_mne_raw(stream, marker_stream, beamformer):
     # Extract information from stream
@@ -144,52 +197,59 @@ def create_mne_raw(stream, marker_stream, beamformer):
     raw.info['temp'] = t_reference
 
     markers = np.array(marker_stream['time_series'])[:, 7]
-    onsets = marker_stream['time_stamps'] - t_reference[0]  # onset from EEG stream or markers stream?
+    onsets = (
+        marker_stream['time_stamps'] - t_reference[0]
+    )  # onset from EEG stream or markers stream?
     annotations = mne.Annotations(onsets, [0] * len(onsets), markers)
     raw.set_annotations(annotations)
 
     return raw
 
-# CHANGE HERE BAD ELECTRODES !!!!
-#default_bads = []
-#default_bads = ['AF3','F5','F7','F3','P3','PO3','P5','C6']
-#bads = default_bads + []
 
-#file_nostim = 'C:/Users/Annalisa Colucci/Desktop/JupyterNotebook/ClosedLoop/Data/MNS_study/P12NoStimulation_02.xdf'
+# CHANGE HERE BAD ELECTRODES !!!!
+# default_bads = []
+# default_bads = ['AF3','F5','F7','F3','P3','PO3','P5','C6']
+# bads = default_bads + []
+
+# file_nostim = 'C:/Users/Annalisa Colucci/Desktop/JupyterNotebook/ClosedLoop/Data/MNS_study/P12NoStimulation_02.xdf'
 root = tk.Tk()
 root.withdraw()
-folder_path = filedialog.askdirectory(initialdir = "C:\\Users\\Annalisa Colucci\\Desktop\\\BCI_softwares\\BeamBCI-ClosedLoopStimulation\\Data\\Force Study Taisiia")
-raw = mne.io.read_raw_brainvision(folder_path+'\\Relax.vhdr',preload=True)
-raw.drop_channels(['envelope','envelope_am','force'])
-#Load bad channel names
-#simulated_beamformer = loadmat('{}\\eeg_env_simulation.mat'.format(folder_path))['to_store'].squeeze()
-forward_model = loadmat('{}\\P_TARGET_64.mat'.format(folder_path))['P_TARGET_64'].squeeze()
+folder_path = filedialog.askdirectory(
+    initialdir="C:\\Users\\Annalisa Colucci\\Desktop\\\BCI_softwares\\BeamBCI-ClosedLoopStimulation\\Data\\Force Study Taisiia"
+)
+raw = mne.io.read_raw_brainvision(folder_path + '\\Relax.vhdr', preload=True)
+raw.drop_channels(['envelope', 'envelope_am', 'force'])
+# Load bad channel names
+# simulated_beamformer = loadmat('{}\\eeg_env_simulation.mat'.format(folder_path))['to_store'].squeeze()
+forward_model = loadmat('{}\\P_TARGET_64.mat'.format(folder_path))[
+    'P_TARGET_64'
+].squeeze()
 mask_bad = forward_model == 0
 mask_good = ~mask_bad
 bads = np.array(raw.ch_names)[:64][mask_bad]
 forwardModel = forward_model[mask_good]
 
-#Extract bad channels from bad_idx file
-#bads_idx = loadmat('{}\\exclude_idx.mat'.format(folder_path))['exclude_idx'].squeeze()
-#bads = []
-#for ch in bads_idx-1:
+# Extract bad channels from bad_idx file
+# bads_idx = loadmat('{}\\exclude_idx.mat'.format(folder_path))['exclude_idx'].squeeze()
+# bads = []
+# for ch in bads_idx-1:
 #    bads.append(raw.ch_names[ch])
 
-good_indices = np.where(~np.isin(raw.ch_names,bads))[0]
+good_indices = np.where(~np.isin(raw.ch_names, bads))[0]
 raw.drop_channels(bads)
-#raw_ref = raw.copy().set_eeg_reference(ref_channels=['Pz'])
+# raw_ref = raw.copy().set_eeg_reference(ref_channels=['Pz'])
 
 
-#Try plotting the online selected forward model
+# Try plotting the online selected forward model
 plot_forward_model(raw, forwardModel)
 forward_model_new = get_ssd_forward_model(raw)
 
 
-#forward_model_cca = get_cca_forward_model(raw)
+# forward_model_cca = get_cca_forward_model(raw)
 
 # Compute backward model - Beamformer
-#Bandpass data into alpha
-raw.filter(10,14)
-beamformer = make_target(raw.data,forward_model_new)
+# Bandpass data into alpha
+raw.filter(10, 14)
+beamformer = make_target(raw.data, forward_model_new)
 
 print('end')
