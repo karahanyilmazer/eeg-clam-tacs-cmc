c2af054959c3f4a5c712c432af7149b2d660b88b
Karahan Yilmazer <karahan.yilmazer@tum.de>
2024-07-10 14:24:30 +0200

    feat: Test MNE SSD

--- test_ssd.py
+++ test_ssd.py
@@ -8,6 +8,7 @@ from os.path import join
 import matplotlib.pyplot as plt
 import mne
 import numpy as np
+from mne.decoding import SSD
 from mne.time_frequency import psd_array_welch
 from mne.viz import plot_topomap
 from mpl_toolkits.axes_grid1 import inset_locator
@@ -145,5 +146,58 @@ eig_vals, eig_vecs = linalg.eig(C_s, C_n)
 M, pf = get_P_TARGET(raw, l_freq, h_freq)
 
 # %%
+freqs_sig = 10, 14
+freqs_noise = 8, 16
 
+ssd = SSD(
+    info=raw.info,
+    reg="oas",
+    filt_params_signal=dict(
+        l_freq=freqs_sig[0],
+        h_freq=freqs_sig[1],
+        l_trans_bandwidth=1,
+        h_trans_bandwidth=1,
+    ),
+    filt_params_noise=dict(
+        l_freq=freqs_noise[0],
+        h_freq=freqs_noise[1],
+        l_trans_bandwidth=1,
+        h_trans_bandwidth=1,
+    ),
+)
+ssd.fit(X=raw.get_data())
+ssd.transform(X=raw.get_data())
+
+# %%
+# Let's investigate spatial filter with max power ratio.
+# We will first inspect the topographies.
+# According to Nikulin et al. 2011 this is done by either inverting the filters
+# (W^{-1}) or by multiplying the noise cov with the filters Eq. (22) (C_n W)^t.
+# We rely on the inversion approach here.
+
+pattern = mne.EvokedArray(data=ssd.patterns_[:4].T, info=ssd.info)
+pattern.plot_topomap(units=dict(mag="A.U."), time_format="")
+
+# The topographies suggest that we picked up a parietal alpha generator.
+
+# Transform
+ssd_sources = ssd.transform(X=raw.get_data())
+
+# Get psd of SSD-filtered signals.
+psd, freqs = mne.time_frequency.psd_array_welch(
+    ssd_sources, sfreq=raw.info["sfreq"], n_fft=4096
+)
+
+# Get spec_ratio information (already sorted).
+# Note that this is not necessary if sort_by_spectral_ratio=True (default).
+spec_ratio, sorter = ssd.get_spectral_ratio(ssd_sources)
+
+# Plot spectral ratio (see Eq. 24 in Nikulin 2011).
+fig, ax = plt.subplots(1)
+ax.plot(spec_ratio, color="black")
+ax.plot(spec_ratio[sorter], color="orange", label="sorted eigenvalues")
+ax.set_xlabel("Eigenvalue Index")
+ax.set_ylabel(r"Spectral Ratio $\frac{P_f}{P_{sf}}$")
+ax.legend()
+ax.axhline(1, linestyle="--")
 # %%
