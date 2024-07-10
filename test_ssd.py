a1f66aaa48bc2835d0025858d518883722c73aeb
Karahan Yilmazer <karahan.yilmazer@tum.de>
2024-07-10 14:24:09 +0200

    feat: Better SSD component plotting

--- test_ssd.py
+++ test_ssd.py
@@ -2,6 +2,9 @@
 # !%matplotlib qt
 # !%load_ext autoreload
 # !%autoreload 2
+import os
+from os.path import join
+
 import matplotlib.pyplot as plt
 import mne
 import numpy as np
@@ -17,7 +20,7 @@ base_dir, cmap, _, _ = get_base_dir(), get_cmap('parula'), set_style(), set_fig_
 
 
 # %%
-def get_P_TARGET(raw, l_freq, h_freq, df):
+def get_P_TARGET(raw, l_freq, h_freq, df, n_comps=4, save=False):
     data_signal = (
         raw.copy()
         .filter(l_freq, h_freq, l_trans_bandwidth=1, h_trans_bandwidth=1)
@@ -40,7 +43,10 @@ def get_P_TARGET(raw, l_freq, h_freq, df):
     D = evecs[:, ix].T
     M = linalg.pinv(D)
 
-    for ix_comp in range(4):
+    if n_comps == 'all':
+        n_comps = M.shape[0]
+
+    for ix_comp in range(n_comps):
         psd, freqs = psd_array_welch(
             D[ix_comp] @ data_broad,
             raw.info['sfreq'],
@@ -51,8 +57,8 @@ def get_P_TARGET(raw, l_freq, h_freq, df):
         freq_mask = np.logical_and(freqs > l_freq, freqs < h_freq)
         peak_freq = freqs[freq_mask][np.argmax(psd[freq_mask])]
 
-        _, ax = plt.subplots()
-        ax.axvline(peak_freq, color='grey', ls='--', lw=0.5)
+        fig, ax = plt.subplots()
+        ax.axvline(peak_freq, color='tomato', ls='--', lw=0.5)
         ax.semilogy(freqs, psd)
 
         axins = inset_locator.inset_axes(
@@ -60,29 +66,67 @@ def get_P_TARGET(raw, l_freq, h_freq, df):
         )
         plot_topomap(M[:, ix_comp], raw.info, axes=axins)
 
-        ax.set_title(f'Component {ix_comp:d}: PF = {peak_freq:.2f} Hz')
+        ax.set_title(f'Component {ix_comp:d}', loc='left')
         ax.set_xlabel('Frequency (Hz)')
         ax.set_ylabel('PSD (dB/Hz)')
-        plt.show()
 
-    return M[:, 3], peak_freq
+        text_str = '\n'.join(
+            (
+                f'$f_S={l_freq}-{h_freq}$ Hz',
+                f'$f_N={l_freq-df}-{h_freq+df}$ Hz',
+            )
+        )
+        props = dict(facecolor='none', edgecolor='black')
+
+        ax_pos = ax.get_position()
+        fig.text(
+            ax_pos.x1 - 0.007,
+            1 - ax_pos.y0,
+            text_str,
+            horizontalalignment='right',
+            verticalalignment='bottom',
+            bbox=props,
+            transform=fig.transFigure,
+        )
+        ax.text(
+            peak_freq + 0.2,
+            0.94,
+            f'$PF={peak_freq:.2f}$ Hz',
+            horizontalalignment='left',
+            verticalalignment='bottom',
+            transform=ax.get_xaxis_transform(),
+            color='tomato',
+        )
+
+        folder_path = join(os.path.dirname(__file__), 'img', f'{l_freq}-{h_freq}Hz')
+        os.makedirs(folder_path, exist_ok=True)
+
+        if save:
+            plt.savefig(
+                join(
+                    folder_path,
+                    f'ssd-{raw.info["subject_info"]["his_id"]}-comp_{ix_comp}.png',
+                ),
+                dpi=300,
+            )
+            plt.close()
+
+        else:
+            plt.show()
+
+    return M, D, peak_freq
 
 
 # %%
-# Read in the data
-raw = read_raw('FS_10')
+for subj in os.listdir('data'):
+    if int(subj.split('_')[1]) >= 9:
+        # Read in the data
+        raw = read_raw(subj)
 
-# Define the SSD parameters
-l_freq, h_freq = 10, 14
-df = 2
-M, pf = get_P_TARGET(raw, l_freq, h_freq, df)
-
-# forward_model = loadmat(join(data_folder, 'P_TARGET_64.mat'))['P_TARGET_64'].squeeze()
-# mask_bad = forward_model == 0
-# mask_good = ~mask_bad
-# bads = np.array(raw.ch_names)[:64][mask_bad]
-# forwardModel = forward_model[mask_good]
-# raw.drop_channels(bads)
+        # Define the SSD parameters
+        l_freq, h_freq = 10, 14
+        df = 2
+        M, D, pf = get_P_TARGET(raw, l_freq, h_freq, df, n_comps='all', save=True)
 
 # %%
 x_s = raw.copy().filter(l_freq, h_freq, l_trans_bandwidth=1, h_trans_bandwidth=1)._data
