620374d489ce370a84a3f6d59b0fe25a72db713f
Karahan Yilmazer <karahan.yilmazer@tum.de>
2024-07-14 12:39:55 +0200

    refactor: Specify the folder for plots to be stored in

--- fooof_test_ssd.py
+++ fooof_test_ssd.py
@@ -1,5 +1,5 @@
 # %%
-# !%matplotlib inline
+# !%matplotlib qt
 # !%load_ext autoreload
 # !%autoreload 2
 import os
@@ -16,25 +16,29 @@ from src.source_space.SSD import SSD
 
 # %%
 # Read in the data
-subj = 'FS_03'
+subj = 'FS_07'
 raw = read_raw(subj)
 
+# Read in configuration values
 with open('config.yaml', 'r') as file:
     config = safe_load(file)
 
-# Initialize the SSD
 l_freq = config['l_freq']
 h_freq = config['h_freq']
 df = config['df']
 alpha_comp = config['alpha_comp'][subj]
 beta_comp = config['beta_comp'][subj]
+base_dir = config['base_dir_win'] if sys.platform == 'win32' else config['base_dir_mac']
+img_folder = 'img'
 
+# Apply the SSD
 ssd = SSD()
 ssd.fit(raw, l_freq, h_freq, df)
-ssd.plot(comp_idx=alpha_comp, prefix='Alpha', save=False)
-ssd.fit_fooof(config, plot=True)
+ssd.plot(n_comps=3, img_folder=img_folder, save=False)
+ssd.fit_fooof(config, plot=False)
 alpha, beta, peaks = ssd.adjust_freq_bands(config['gauss_thr'][subj], plot=False)
 
+# Store the resulting frequency parameters
 config['alpha_range'][subj] = [float(freq) for freq in alpha]
 config['beta_range'][subj] = [float(freq) for freq in beta]
 config['alpha_peak'][subj] = float(ssd.fm.freqs[peaks[0]])
@@ -44,12 +48,13 @@ with open('config.yaml', 'w') as file:
     safe_dump(config, file, sort_keys=False)
 
 # %%
+# Fit the SSD again on individualized alpha and beta ranges
 ssd_alpha = SSD()
 ssd_alpha.fit(raw, alpha[0], alpha[1], df)
-ssd_alpha.plot(comp_idx=alpha_comp, prefix='Alpha', save=False)
+ssd_alpha.plot(comp_idx=alpha_comp, prefix='Alpha', img_folder=img_folder, save=False)
 
-# %%
 ssd_beta = SSD()
 ssd_beta.fit(raw, beta[0], beta[1], df)
-ssd_beta.plot(comp_idx=beta_comp, prefix='Beta', save=False)
+ssd_beta.plot(comp_idx=beta_comp, prefix='Beta', img_folder=img_folder, save=False)
+
 # %%
