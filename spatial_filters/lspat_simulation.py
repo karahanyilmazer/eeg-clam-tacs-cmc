38d43c802b0954280fdf55ed83911ea4e5483145
Karahan Yilmazer <yilmazerkarahan@gmail.com>
2024-08-31 00:17:26 +0200

    feat: Compute the phase-locking value

--- spatial_filters/lspat_simulation.py
+++ spatial_filters/lspat_simulation.py
@@ -25,7 +25,26 @@ from src.source_space.SSD import SSD
 plt.rcParams.update({'figure.dpi': 300})
 plt.style.use(['science', 'no-latex'])
 
+
 # %%
+def compute_plv(signal1, signal2):
+    # Compute the analytic signal using the Hilbert transform
+    analytic_signal1 = hilbert(signal1)
+    analytic_signal2 = hilbert(signal2)
+
+    # Extract the instantaneous phase
+    phase1 = np.angle(analytic_signal1)
+    phase2 = np.angle(analytic_signal2)
+
+    # Compute the phase difference
+    phase_diff = phase1 - phase2
+
+    # Compute the Phase Locking Value (PLV)
+    plv = np.abs(np.mean(np.exp(1j * phase_diff)))
+
+    return plv
+
+
 # Load data from the mat file
 mat = loadmat('emptyEEG.mat')
 EEG = mat['EEG'][0, 0]
@@ -35,6 +54,7 @@ orig_EEG = EEG.copy()
 
 # Filter parameters
 freqs = np.logspace(np.log10(8), np.log10(80), 10)
+# freqs = np.array([11, 22, 33])
 fwhm_filt = 2
 fwhm_anal = 5
 
@@ -55,6 +75,7 @@ ch_names = [el[0] for el in EEG['chanlocs']['labels'][0]]
 spat_maps = np.zeros((n_ch, len(freqs), len(filters), 2))
 corr_data = np.full((len(freqs), len(filters), 2), np.nan)
 snrs = np.full((len(freqs), len(filters), 2), np.nan)
+plvs = np.full((len(freqs), len(filters), 2), np.nan)
 xmin, xmax = EEG['xmin'].item(), EEG['xmax'].item()
 t_idx = np.searchsorted(times, [xmin + 0.5, xmax - 0.5])
 
@@ -146,6 +167,8 @@ for fi, freq in enumerate(freqs):
         tc_data = filterFGx(EEG['data'][max_el, :], srate, freq, fwhm_anal)[0]
         corr_data[fi, filt_num, noise_i] = np.corrcoef(tc_data, signal1)[0, 1] ** 2
 
+        plvs[fi, filt_num, noise_i] = compute_plv(tc_data, signal1)
+
         f = np.abs(fft(EEG['data'][max_el, :]) / EEG['pnts']) ** 2
         snrs[fi, filt_num, noise_i] = f[freq_idx] / np.mean(f[np.r_[f_low, f_high]])
 
@@ -165,6 +188,8 @@ for fi, freq in enumerate(freqs):
         corr_data[fi, filt_num, noise_i] = (
             np.corrcoef(pca_data[best_comp, :], signal1)[0, 1] ** 2
         )
+
+        plvs[fi, filt_num, noise_i] = compute_plv(pca_data[best_comp, :], signal1)
 
         pca_data = EEG['data'].T @ pca_vecs[:, idx]
         f = np.abs(fft(pca_data) / EEG['pnts']) ** 2
@@ -196,6 +221,8 @@ for fi, freq in enumerate(freqs):
 
         corr_data[fi, filt_num, noise_i] = np.corrcoef(jd_data, signal1)[0, 1] ** 2
 
+        plvs[fi, filt_num, noise_i] = compute_plv(jd_data, signal1)
+
         jd_data = EEG['data'].T @ jdw[:, max_comp]
         f = np.abs(fft(jd_data) / EEG['pnts']) ** 2
         snrs[fi, filt_num, noise_i] = f[freq_idx] / np.mean(f[np.r_[f_low, f_high]])
@@ -218,6 +245,8 @@ for fi, freq in enumerate(freqs):
         spat_maps[:, fi, filt_num, noise_i] = maps * np.sign(maps[idx])
 
         corr_data[fi, filt_num, noise_i] = np.corrcoef(ged_data, signal1)[0, 1] ** 2
+
+        plvs[fi, filt_num, noise_i] = compute_plv(ged_data, signal1)
 
         ged_data = EEG['data'].T @ ged_vecs[:, max_idx]
         f = np.abs(fft(ged_data) / EEG['pnts']) ** 2
@@ -306,6 +335,7 @@ for fi, freq in enumerate(freqs):
         idx = np.argmax(np.abs(A[:, 0]))
         spat_maps[:, fi, filt_num, noise_i] = A[:, 0] * np.sign(A[idx, 0])
         corr_data[fi, filt_num, noise_i] = np.corrcoef(X_ssd[0, :], signal1)[0, 1] ** 2
+        plvs[fi, filt_num, noise_i] = compute_plv(X_ssd[0, :], signal1)
         f = np.abs(fft(X_ssd[0, :]) / EEG['pnts']) ** 2
         snrs[fi, filt_num, noise_i] = f[freq_idx] / np.mean(f[np.r_[f_low, f_high]])
 
@@ -378,6 +408,27 @@ for i in range(2):
     axs[i].legend(filters.keys())
 
 plt.savefig(os.path.join('img', 'snr.png'))
+plt.show()
+
+# %%
+fig, axs = plt.subplots(2, 1, figsize=(8, 6))
+
+for i in range(2):
+    axs[i].plot(
+        freqs, plvs[:, :, i], 's-', linewidth=2, markersize=10, markerfacecolor='w'
+    )
+    if i == 1:
+        axs[i].set_title('O1 Noise')
+        axs[i].set_xlabel('Frequency (Hz)')
+    else:
+        axs[i].set_title('No Noise')
+    axs[i].set_ylabel('Phase Locking Value')
+    axs[i].set_xlim([freqs[0] - 0.1, freqs[-1] + 1])
+    axs[i].set_ylim([0, 1])
+    axs[i].set_xscale('log')
+    axs[i].legend(filters.keys())
+
+plt.savefig(os.path.join('img', 'plv.png'))
 plt.show()
 
 # %%
